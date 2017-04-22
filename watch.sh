21af2af54e78662e67dc7b243e918a4c46d4a17e
Sylvain Leroux <sylvain@chicoree.fr>
2017-04-22 14:25:36 +0200

    Hide/show cursor to avoid blinking

--- watch.sh
+++ watch.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 while true; do
     {
-        echo -n $'\033[2J\033[H'
+        echo -n $'\033[?25l\033[2J\033[H'
         echo 'ls -x' | bash -i 2>&1
-    } > /tmp/o
-    sed -e /exit/d /tmp/o
+        echo -n $'\033[?25h' # Should 'trap' that to always
+                             # restore cursor on exit
+    } | sed -e /exit/d
     sleep 0.5
 done
