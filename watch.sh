27a47d7d02c62214d80f12feb68a4a9eb479607e
Yes I Know IT <yesik@bulbizarre.kanto.pkpm>
2017-05-04 15:52:34 +0200

    Add more hidden files examples

--- watch.sh
+++ watch.sh
@@ -2,7 +2,7 @@
 while true; do
     {
         echo -n $'\033[?25l\033[2J\033[H'
-        echo 'ls -x' | bash -i 2>&1
+        echo 'ls -ax' | bash -i 2>&1
         echo -n $'\033[?25h' # Should 'trap' that to always
                              # restore cursor on exit
     } | sed -e /exit/d
